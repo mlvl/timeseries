2cf2dd63cc0fc28aec5601cd5f2a05b51f977f92
meredithmmyers <myers.mm89@gmail.com>
2015-02-12 11:26:49 -0500

    Remove old code

--- timeseries.js
+++ timeseries.js
@@ -1,140 +1,77 @@
 (function() {
-var margin = {top: 10, right: 10, bottom: 100, left: 40},
-    margin2 = {top: 430, right: 10, bottom: 20, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
-    height2 = 500 - margin2.top - margin2.bottom;
+    var margin = {
+            top: 10,
+            right: 10,
+            bottom: 100,
+            left: 40
+        },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+        var parseDate = d3.time.format("%b %Y").parse;
 
-var parseDate = d3.time.format("%b %Y").parse;
+    function type(d) {
+        d.date = parseDate(d.date);
+        d.price = +d.price;
+        return d;
+    }
+    d3.csv("sp500.csv", type, function(error, data) {
 
-var x = d3.time.scale().range([0, width]),
-    x2 = d3.time.scale().range([0, width]),
-    y = d3.scale.linear().range([height, 0]),
-    y2 = d3.scale.linear().range([height2, 0]);
+        var x = d3.time.scale().range([0, width]),
+            y = d3.scale.linear().range([height, 0]);
 
-var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-    xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
-    yAxis = d3.svg.axis().scale(y).orient("left");
+        var xAxis = d3.svg.axis().scale(x).orient("bottom")
+            //.ticks(10)
+            .tickSize(-height, 0)
+            .tickFormat(d3.time.format("%m/%d/%y"));
 
-var brush = d3.svg.brush()
-    .x(x2)
-    .on("brush", brushed);
+        var yAxis = d3.svg.axis().scale(y).orient("left")
+            //xAxis.ticks(5)
+            .tickSize(-width + margin.right, margin.left)
+            .tickFormat(d3.time.format("%H:%M"));
 
-var area = d3.svg.area()
-    .interpolate("monotone")
-    .x(function(d) { return x(d.date); })
-    .y0(height)
-    .y1(function(d) { return y(d.price); });
+        // x.domain(d3.extent([padded.minDate, padded.maxDate]));
+        // y.domain(d3.extent([stop, start]));
+        x.domain(d3.extent(data.map(function(d) {
+            return d.date;
+        })));
+        y.domain([0, d3.max(data.map(function(d) {
+            return d.price;
+        }))]);
 
-var area2 = d3.svg.area()
-    .interpolate("monotone")
-    .x(function(d) { return x2(d.date); })
-    .y0(height2)
-    .y1(function(d) { return y2(d.price); });
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+        var context = svg.append("g")
+            .attr("class", "context")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.append("defs").append("clipPath")
-    .attr("id", "clip")
-  .append("rect")
-    .attr("width", width)
-    .attr("height", height);
+        context.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(5," + (height - margin.bottom) + ")")
+            .call(xAxis);
 
-var focus = svg.append("g")
-    .attr("class", "focus")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        context.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(40," + 0 + ")")
+            .call(yAxis);
 
-var context = svg.append("g")
-    .attr("class", "context")
-    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-
-var zoom = d3.behavior.zoom()
-    .on("zoom", draw);
-// Add rect cover the zoomed graph and attach zoom event.
-var rect = svg.append("svg:rect")
-    .attr("class", "pane")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    .call(zoom);
-
-d3.csv("sp500.csv", type, function(error, data) {
-  x.domain(d3.extent(data.map(function(d) { return d.date; })));
-  y.domain([0, d3.max(data.map(function(d) { return d.price; }))]);
-  x2.domain(x.domain());
-  y2.domain(y.domain());
-
-  // Set up zoom behavior
-  zoom.x(x);
-
-  focus.append("path")
-      .datum(data)
-      .attr("class", "area")
-      .attr("d", area);
-
-  focus.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  focus.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
-
-  context.append("path")
-      .datum(data)
-      .attr("class", "area")
-      .attr("d", area2);
-
-  context.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height2 + ")")
-      .call(xAxis2);
-
-  context.append("g")
-      .attr("class", "x brush")
-      .call(brush)
-    .selectAll("rect")
-      .attr("y", -6)
-      .attr("height", height2 + 7);
-});
-
-function brushed() {
-  x.domain(brush.empty() ? x2.domain() : brush.extent());
-  focus.select(".area").attr("d", area);
-  focus.select(".x.axis").call(xAxis);
-  // Reset zoom scale's domain
-  zoom.x(x);
-}
-
-function draw() {
-  focus.select(".area").attr("d", area);
-  focus.select(".x.axis").call(xAxis);
-  // Force changing brush range
-  brush.extent(x.domain());
-  svg.select(".brush").call(brush);
-}
-
-function type(d) {
-  d.date = parseDate(d.date);
-  d.price = +d.price;
-  return d;
-}
-  $(function() {
-        $( "#slider-range" ).slider({
-            range: true,
-            min: 0,
-            max: 1000,
-            values: [ 0, 1000 ],
-            slide: function( event, ui ) {
-              var begin = d3.min([ui.values[0], data.length]);
-              var end = d3.max([ui.values[1], 0]);
-              console.log("begin:", begin, "end:", end);
-
-              zoom(begin, end);
-            }
-        });
+        context.selectAll(".circ")
+            .data(data)
+            .enter().append("circle")
+            .attr("class", "circ")
+            .attr("cx", function(d) {
+              console.log(d)
+                return x(getDate(d.value));
+            })
+            .attr("cy", function(d, i) {
+                return y(getTime(d.value));
+            })
+            .on('click', function(d) {
+                //console.log(new Date(d.value))
+            })
+            .attr("r", 7);
     });
+
+
 })();
